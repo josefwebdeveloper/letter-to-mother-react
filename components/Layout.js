8a8b4d79e77b562b8cdb0395680345cbb3718185
josephs <josef2011@gmail.com>
2020-05-23 23:52:14 +0300

    test slider

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ const Layout = (props) => (
             name="description"
             content="letter to mother"
         />
+
       {/*<link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>*/}
         <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap" rel="stylesheet"/>
